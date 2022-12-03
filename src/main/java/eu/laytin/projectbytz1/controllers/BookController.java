b4255204b9743d43b4691f674f2b5310fda4eb52
Laytin <xpor2002@gmail.com>
2022-12-04 01:01:56 +0300

    Book: id,asign and release

--- src/main/java/eu/laytin/projectbytz1/controllers/BookController.java
+++ src/main/java/eu/laytin/projectbytz1/controllers/BookController.java
@@ -1,7 +1,83 @@
 package eu.laytin.projectbytz1.controllers;
 
+import eu.laytin.projectbytz1.dao.BookDAO;
+import eu.laytin.projectbytz1.dao.PersonDAO;
+import eu.laytin.projectbytz1.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/book")
 public class BookController {
+    private final BookDAO bookDAO;
+    private final PersonDAO personDAO;
+    @Autowired
+    public BookController(BookDAO bookDAO, PersonDAO personDAO) {
+        this.bookDAO = bookDAO;
+        this.personDAO = personDAO;
+    }
+    @GetMapping()
+    public String index(Model model){
+        model.addAttribute("books",bookDAO.showAll());
+        return "book/index";
+    }
+
+    @GetMapping("/{id}")
+    public String id(@PathVariable("id") int id, @ModelAttribute("person") Person person, Model model){
+        model.addAttribute("book",bookDAO.showIndex(id));
+        Optional<Person> owner;
+        if((owner=bookDAO.getBookOwner(id)).isPresent()){
+            model.addAttribute("owner", owner.get());
+        }else{
+            model.addAttribute("persons",personDAO.showAll());
+        }
+        return "book/id";
+    }
+
+    @PatchMapping("/{id}/resetOwner")
+    public String resetOwner(@PathVariable("id") int id){
+        bookDAO.resetOwner(id);
+        return "redirect:/book/{id}";
+    }
+
+    @PatchMapping("/{id}/setOwner")
+    public String setOwner(@PathVariable("id") int id, @ModelAttribute("person") Person person){
+        bookDAO.setOwner(id,person);
+        return "redirect:/book/{id}";
+    }
+
+//    @PostMapping()
+//    public String createPost(@ModelAttribute("person") @Valid Person person,
+//                             BindingResult bindingResult){
+//        if(bindingResult.hasErrors()){
+//            return "person/new";
+//        }
+//        personDAO.createPerson(person);
+//        return "redirect:/person";
+//    }
+//    @GetMapping("/{id}/edit")
+//    public String edit(@PathVariable("id") int id,Model model){
+//        model.addAttribute("person", personDAO.showIndex(id));
+//        return "person/edit";
+//    }
+//
+//    @PatchMapping("/{id}")
+//    public String update(@PathVariable("id") int id,@ModelAttribute("person") @Valid Person person,
+//                         BindingResult br){
+//        if(br.hasErrors()){
+//            return "person/edit";
+//        }
+//        personDAO.updatePerson(id,person);
+//        return "redirect:/person";
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public String delete(@PathVariable("id") int id){
+//        personDAO.deletePerson(id);
+//        return "redirect:/person";
+//    }
 }
--- src/main/java/eu/laytin/projectbytz1/dao/BookDAO.java
+++ src/main/java/eu/laytin/projectbytz1/dao/BookDAO.java
@@ -7,6 +7,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
+
 @Component
 public class BookDAO {
     private final JdbcTemplate jbdc;
@@ -22,21 +24,32 @@ public class BookDAO {
 
     //Показать с id
     public Book showIndex(int book_id){
-        return jbdc.query("SELECT * FROM Book WHERE book_id=?", new Object[book_id],
+        return jbdc.query("SELECT * FROM Book WHERE id=?", new Object[]{book_id},
                 new BeanPropertyRowMapper<Book>(Book.class)).stream().findAny().orElse(null);
     }
 
     //Создать нового
     public void createBook(Book book){
-        jbdc.update("INSERT INTO Book(book_name,book_author,book_year) VALUES(?,?,?)", book.getName(),book.getAuthor(), book.getYear());
+        jbdc.update("INSERT INTO Book(name,author,year) VALUES(?,?,?)", book.getName(),book.getAuthor(), book.getYear());
     }
 
     //Обновить существующего (Попробовать убрать из аргументов id и юзать person.getID())
-    public void updatePerson(int id,Book book){
-        jbdc.update("UPDATE Book SET name=?,author=?,year=? WHERE book_id=?",book.getName(),book.getAuthor(),book.getYear(),id);
+    public void updateBook(int id,Book book){
+        jbdc.update("UPDATE Book SET name=?,author=?,year=? WHERE id=?",book.getName(),book.getAuthor(),book.getYear(),id);
     }
     //Удалить с id
-    public void deletePerson(int id){
-        jbdc.update("DELETE From Book WHERE book_id=?",id);
+    public void deleteBook(int id){
+        jbdc.update("DELETE From Book WHERE id=?",id);
+    }
+
+    public void resetOwner(int bookId){
+        jbdc.update("UPDATE Book SET person_id=null WHERE id=?",bookId);
+    }
+    public void setOwner(int id, Person person){
+        jbdc.update("UPDATE Book SET person_id=? WHERE id=?",person.getId(),id);
+    }
+    public Optional<Person> getBookOwner(int id){
+        return jbdc.query("SELECT Person.* FROM Book JOIN person on person.id = book.person_id WHERE book.id=?",
+                new Object[]{id},new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
     }
 }
